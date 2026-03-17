e85aac0fe2280444f961c6b3467ea74c07d2950c
snehshah102 <snehshah2013@gmail.com>
2026-03-17 16:23:09 -0400

    update docker

--- frontend/app/planes/page.tsx
+++ frontend/app/planes/page.tsx
@@ -1,6 +1,8 @@
 import { PlaneIndex } from "@/components/plane/plane-index";
 import { getLivePlanesPayload } from "@/lib/live-plane-summaries";
 
+export const dynamic = "force-dynamic";
+
 export default async function PlanesPage() {
   const initialData = await getLivePlanesPayload();
   return <PlaneIndex initialPlanes={initialData.planes} />;
